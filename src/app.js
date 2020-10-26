4cdee4f5d76821245bbe78a865625ee7b946dbfc
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-10-26 12:00:14 -0300

    graphql api study

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const server = new ApolloServer({
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 })
 
 export default server
